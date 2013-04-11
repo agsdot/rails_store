089b042da1d106f4abf7824ae67407fd376abfe7
Jonathan Lai <jonathanlai@eltux.net>
2013-04-10 17:34:33 -0700

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStore::Application.config.secret_token = 'e4066b005d65edeb6ad7ee3c5a7ab98931c5a210af71d5d5df3f7bff96d3eff24d609182c5de917dc818ac4eca52f8f72d16febb8d5a7e9c89cf6ff864b87ec2'
