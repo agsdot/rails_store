d5baa6288bdf143ddfcdca989d889a449acc9cff
Jonathan Lai <jonathanlai@eltux.net>
2013-04-14 21:00:39 -0700

    4/14/13 2100pm save, models changed validations put in

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :description, :image_url, :in_cart
 
   has_many :reviews
+  validates :name, :uniqueness => true
+  validates :name, :price, :presence => true
+
+
 end
